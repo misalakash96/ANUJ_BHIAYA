30cc3b537fabd61d36f470b593908e5883177f8b
Akash Misal <misalakash2796@gmail.com>
2023-10-06 10:39:54 +0530

    initial commit

---
+++ SumTwoNumbers.java
@@ -0,0 +1,13 @@
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        // Declare and initialize the two numbers
+        int a = 5;
+        int b = 7;
+
+        // Calculate the sum
+        int sum = a + b;
+
+        // Display the result
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
