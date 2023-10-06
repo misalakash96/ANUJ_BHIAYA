44dcb0380474e76232e19c4ca75a9a5ccb427555
Akash Misal <misalakash2796@gmail.com>
2023-10-07 00:24:33 +0530

    Multiply.java Added

---
+++ multiply.java
@@ -0,0 +1,13 @@
+public class multiply {
+    public static void main(String[] args) {
+        // Declare and initialize the two numbers
+        int a = 5;
+        int b = 7;
+
+        // Calculate the sum
+        int sum = a * b;
+
+        // Display the result
+        System.out.println("The multiply of given numbers " + a + " and " + b + " is: " + sum);
+    }
+}
